eb97165295899bcafc54b52a49a748a7bd964273
HeshamD <heshameldawy__@outlook.com>
2022-06-29 05:26:36 -0400

    Updating the controllers

---
+++ CarDealerShip/Controllers/AboutController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+/// <summary>
+/// Contact us 
+/// About Us
+/// Our Team
+/// </summary>
+namespace CarDealerShip.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AboutController : ControllerBase
+    {
+    }
+}
---
+++ CarDealerShip/Controllers/VehicleInventoryController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+/// <summary>
+/// Special Offers Vehicle
+/// </summary>
+
+namespace CarDealerShip.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehicleInventoryController : ControllerBase
+    {
+    }
+}
